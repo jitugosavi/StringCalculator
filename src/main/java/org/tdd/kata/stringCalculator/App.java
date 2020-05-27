a1d4e1fd396bbaa2ef324d44549872c1d3c18be5
Jitu <Jitu@localhost>
2020-05-27 23:02:36 +0530

    Test for Empty String

--- src/main/java/org/tdd/kata/stringCalculator/App.java
+++ src/main/java/org/tdd/kata/stringCalculator/App.java
@@ -2,5 +2,11 @@ package org.tdd.kata.stringCalculator;
 
 public class App 
 {
-    
+    public static int add(String number) {
+    	if (number == null || number.length() <= 0) {
+    		return 0;
+    	}
+    	
+    	return -1;
+    }
 }
--- src/test/java/org/tdd/kata/stringCalculator/AppTest.java
+++ src/test/java/org/tdd/kata/stringCalculator/AppTest.java
@@ -1,9 +1,16 @@
 package org.tdd.kata.stringCalculator;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest
 {
-	
+	@Test
+	public void shouldReturnZeroOnEmptyInput() {
+		assertEquals(0, App.add(""));
+	}
 }
